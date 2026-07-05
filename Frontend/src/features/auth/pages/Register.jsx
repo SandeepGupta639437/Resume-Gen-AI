1f70be253e5c7269b3632a0eb93e0b3a9beefcac
SandeepGupta639437 <sandeepguptag18@gmail.com>
2026-07-05 09:48:40 +0530

    login-register page and api services

---
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {useNavigate , Link} from "react-router"
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+}
+
+const Register = () => {
+  const navigate = useNavigate();
+
+
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Register</h1>
+        <form onSubmit = {handleSubmit}>
+            <div className="input-group">
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" name="username" placeholder='Enter your username' required />
+            </div>
+            <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder='email.com' required />
+            </div>
+            <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" placeholder='Enter your password' required />
+            </div>
+            <button className="button primary-button" type="submit">
+              Register
+            </button>
+        </form>
+        <p>Already have an account? <Link to="/login">Login</Link></p>
+      </div>
+    </main>
+  )
+}
+
+export default Register
