8da365d69dab302f0919eedbce4cddd5bcdba2a6
SandeepGupta639437 <sandeepguptag18@gmail.com>
2026-09-06 02:43:31 +0530

    Fix frontend route import casing

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -2,7 +2,7 @@ import {createBrowserRouter} from "react-router";
 import Register from "./features/auth/pages/Register";
 import Login from "./features/auth/pages/Login";
 import Protected from "./features/auth/components/Protected.jsx";
-import Home from "./features/interview/pages/home.jsx";
+import Home from "./features/interview/pages/Home.jsx";
 
 export const router = createBrowserRouter([
   {
